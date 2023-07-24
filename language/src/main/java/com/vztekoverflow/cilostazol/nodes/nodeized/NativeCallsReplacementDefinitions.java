f14d4c8c7f29ab6e2b560df092972e6394e13475
Jan Kleprlík <kleprlikjan@gmail.com>
2023-07-24 17:24:05 +0200

    #62 delete unused files

--- language/src/main/java/com/vztekoverflow/cilostazol/nodes/nodeized/NativeCallsReplacementDefinitions.java
+++
@@ -1,9 +0,0 @@
-package com.vztekoverflow.cilostazol.nodes.nodeized;
-
-import com.vztekoverflow.cilostazol.runtime.symbols.MethodSymbol;
-
-public class NativeCallsReplacementDefinitions {
-  public static MethodSymbol ConsoleWriteLine() {
-    return null;
-  }
-}
