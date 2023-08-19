4d3c2bbafbaade48da9d038e4da8c3b5abc59f82
leskovde <leskovar.denis@seznam.cz>
2023-08-19 17:13:54 +0200

    #132: Add simple benchmark

--- tests/src/test/java/com/vztekoverflow/cilostazol/tests/BenchmarkValidityTests.java
+++ tests/src/test/java/com/vztekoverflow/cilostazol/tests/BenchmarkValidityTests.java
@@ -163,4 +163,83 @@ public class BenchmarkValidityTests extends TestBase {
     assertEquals(
         "-0.16907516382852447\n-0.169087605234606\n", result.output().replace("\r\n", "\n"));
   }
+
+  @Test
+  public void simple() {
+    var result =
+        runTestFromCode(
+            """
+              /* The Computer Language Benchmarks Game
+                 https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
+
+                 line-by-line from Greg Buchholz's C program
+              */
+
+              using System;
+
+              class Simple
+              {
+                  public static void Main(String[] args)
+                  {
+                      int w, h, x, y, bit_num = 0;
+                      int byte_acc = 0;
+                      int i, iter = 50;
+                      double limit = 2.0;
+                      double Zr, Zi, Cr, Ci, Tr, Ti;
+                      w = h = 20;
+
+                      Console.WriteLine("P4");
+                      Console.WriteLine(w + " " + h);
+
+                      for (y = 0; y < h; y++)
+                      {
+                          for (x = 0; x < w; x++)
+                          {
+                              Zr = 0.0;
+                              Zi = 0.0;
+                              Cr = (2 * (double) x / w - 1.5);
+                              Ci = (2 * (double) y / h - 1);
+
+                              for (i = 0; i < iter; i++)
+                              {
+                                  Tr = Zr * Zr - Zi * Zi + Cr;
+                                  Ti = 2 * Zr * Zi + Ci;
+                                  Zr = Tr;
+                                  Zi = Ti;
+                                  if (Zr * Zr + Zi * Zi > limit * limit)
+                                      break;
+                              }
+
+                              if (Zr * Zr + Zi * Zi > limit * limit)
+                                  byte_acc = (byte_acc << 1) | 0x00;
+                              else
+                                  byte_acc = (byte_acc << 1) | 0x01;
+
+                              bit_num++;
+
+                              if (bit_num == 8)
+                              {
+                                  Console.Write((byte) byte_acc);
+                                  byte_acc = 0;
+                                  bit_num = 0;
+                              }
+                              else if (x == w - 1)
+                              {
+                                  byte_acc = byte_acc << (8 - w % 8);
+                                  Console.Write((byte) byte_acc);
+                                  byte_acc = 0;
+                                  bit_num = 0;
+                              }
+                          }
+                      }
+                  }
+              }
+              """);
+    assertEquals(
+        """
+                    P4
+                    20 20
+                    010000060060047128063224012722401272241425522431255224255255192312552241425522401272240127224063224047128060060000""",
+        result.output().replace("\r\n", "\n"));
+  }
 }
