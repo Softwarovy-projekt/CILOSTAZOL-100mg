21aa94f622ab6dc53a15a776c14cb60b70467157
leskovde <leskovar.denis@seznam.cz>
2023-08-19 17:10:10 +0200

    #132: Add toString implementations for primitives

--- language/src/main/java/com/vztekoverflow/cilostazol/nodes/internal/ObjectMethodImplementations.java
+++ language/src/main/java/com/vztekoverflow/cilostazol/nodes/internal/ObjectMethodImplementations.java
@@ -5,10 +5,59 @@ import com.vztekoverflow.cilostazol.runtime.context.CILOSTAZOLContext;
 import com.vztekoverflow.cilostazol.runtime.objectmodel.StaticObject;
 
 public final class ObjectMethodImplementations {
-  public static Object ObjectToString(VirtualFrame frame) {
+  public static Object objectToString(VirtualFrame frame) {
     StaticObject object = (StaticObject) frame.getArguments()[0];
     return CILOSTAZOLContext.get(null)
         .getAllocator()
         .createString(object.getTypeSymbol().toString(), frame, 0);
+  }
+
+  public static Object byteToString(VirtualFrame frame) {
+    byte value =
+        (byte)
+            IndirectLoader.loadByte(
+                (StaticObject) frame.getArguments()[0], CILOSTAZOLContext.get(null));
+    return CILOSTAZOLContext.get(null).getAllocator().createString(Byte.toString(value), frame, 0);
+  }
+
+  public static Object shortToString(VirtualFrame frame) {
+    short value =
+        (short)
+            IndirectLoader.loadShort(
+                (StaticObject) frame.getArguments()[0], CILOSTAZOLContext.get(null));
+    return CILOSTAZOLContext.get(null).getAllocator().createString(Short.toString(value), frame, 0);
+  }
+
+  public static Object int32ToString(VirtualFrame frame) {
+    int value =
+        IndirectLoader.loadInt32(
+            (StaticObject) frame.getArguments()[0], CILOSTAZOLContext.get(null));
+    return CILOSTAZOLContext.get(null)
+        .getAllocator()
+        .createString(Integer.toString(value), frame, 0);
+  }
+
+  public static Object int64ToString(VirtualFrame frame) {
+    long value =
+        IndirectLoader.loadInt64(
+            (StaticObject) frame.getArguments()[0], CILOSTAZOLContext.get(null));
+    return CILOSTAZOLContext.get(null).getAllocator().createString(Long.toString(value), frame, 0);
+  }
+
+  public static Object floatToString(VirtualFrame frame) {
+    float value =
+        (float)
+            IndirectLoader.loadFloat(
+                (StaticObject) frame.getArguments()[0], CILOSTAZOLContext.get(null));
+    return CILOSTAZOLContext.get(null).getAllocator().createString(Float.toString(value), frame, 0);
+  }
+
+  public static Object doubleToString(VirtualFrame frame) {
+    double value =
+        IndirectLoader.loadDouble(
+            (StaticObject) frame.getArguments()[0], CILOSTAZOLContext.get(null));
+    return CILOSTAZOLContext.get(null)
+        .getAllocator()
+        .createString(Double.toString(value), frame, 0);
   }
 }
