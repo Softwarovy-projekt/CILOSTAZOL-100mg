e476a2190032d59a6e0cd127ce4a06975834eaf6
leskovde <leskovar.denis@seznam.cz>
2023-08-20 13:31:26 +0200

    #132: Add binaryTree benchmark

--- tests/src/test/java/com/vztekoverflow/cilostazol/tests/BenchmarkValidityTests.java
+++ tests/src/test/java/com/vztekoverflow/cilostazol/tests/BenchmarkValidityTests.java
@@ -242,4 +242,99 @@ public class BenchmarkValidityTests extends TestBase {
                     010000060060047128063224012722401272241425522431255224255255192312552241425522401272240127224063224047128060060000""",
         result.output().replace("\r\n", "\n"));
   }
+
+  @Test
+  public void binaryTrees() {
+    var result =
+        runTestFromCode(
+            """
+          // The Computer Language Benchmarks Game
+          // https://benchmarksgame-team.pages.debian.net/benchmarksgame/
+          //
+          // based Jarkko Miettinen Java #2 and Anthony Lloyd C#\s
+          // contributed by Isaac Gouy
+
+          using System;
+
+          class BinaryTrees
+          {
+              const int MinDepth = 4;
+              const int NoTasks = 4;
+
+              public static void Main(string[] args)
+              {
+                  int maxDepth = 10;
+
+                  Console.WriteLine(string.Concat("stretch tree of depth ", maxDepth + 1,
+                      "\\t check: ", (TreeNode.bottomUpTree(maxDepth + 1)).itemCheck()));
+
+                  var longLivedTree = TreeNode.bottomUpTree(maxDepth);
+
+                  var results = new string[(maxDepth - MinDepth) / 2 + 1];
+
+                  for (int i = 0; i < results.Length; i++)
+                  {
+                      int depth = i * 2 + MinDepth;
+                      int n = (1 << maxDepth - depth + MinDepth) / NoTasks;
+                      var check = 0;
+                      for (int t = 0; t < NoTasks; t++)
+                      {
+                          for (int j = n; j > 0; j--)
+                              check += (TreeNode.bottomUpTree(depth)).itemCheck();
+                      }
+
+                      results[i] = string.Concat(n * NoTasks, "\\t trees of depth ",
+                          depth, "\\t check: ", check);
+                  }
+
+                  for (int i = 0; i < results.Length; i++)
+                      Console.WriteLine(results[i]);
+
+                  Console.WriteLine(string.Concat("long lived tree of depth ", maxDepth,
+                      "\\t check: ", longLivedTree.itemCheck()));
+              }
+
+              private class TreeNode
+              {
+                  readonly TreeNode left, right;
+
+                  internal static TreeNode bottomUpTree(int depth)
+                  {
+                      if (depth > 0)
+                      {
+                          return new TreeNode(
+                              bottomUpTree(depth - 1),
+                              bottomUpTree(depth - 1));
+                      }
+                      else
+                      {
+                          return new TreeNode(null, null);
+                      }
+                  }
+
+                  internal TreeNode(TreeNode left, TreeNode right)
+                  {
+                      this.left = left;
+                      this.right = right;
+                  }
+
+                  internal int itemCheck()
+                  {
+                      if (left == null) return 1;
+                      else return 1 + left.itemCheck() + right.itemCheck();
+                  }
+              }
+          }
+          """);
+    assertEquals(
+        """
+                    stretch tree of depth 11\t check: 4095
+                    1024\t trees of depth 4\t check: 31744
+                    256\t trees of depth 6\t check: 32512
+                    64\t trees of depth 8\t check: 32704
+                    16\t trees of depth 10\t check: 32752
+                    long lived tree of depth 10\t check: 2047
+                    """,
+        result.output().replace("\r\n", "\n"));
+  }
 }
